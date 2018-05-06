cdaf9f5ea815106436eac35071e3bb44b514ef3b
George Tourtsinakis <george@besmartbesimple.com>
2018-05-06 13:30:06 +0300

    Inital commit

---
+++ Assets/Scripts/General/LevelManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+   
+    Scene currentScene;
+    string sceneName;
+
+    float secondsToLoad = 3;
+
+    private void Start()
+    {
+        
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+
+        //Load start screen after splassh screen some seconds
+        if (sceneName == "Splash")
+        {
+            StartCoroutine(CountDown());
+        }
+    }
+
+
+    public void LoadLevel(string name)
+	{
+        //Load level according to parameter
+        SceneManager.LoadScene(name);
+
+    }
+
+	public void QuitRequest()
+	{
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel()
+    {
+        
+        SceneManager.LoadScene(currentScene.buildIndex + 1);
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(secondsToLoad);
+        LoadNextLevel();
+    }
+
+    //Open external link at click
+    public void OpenLinkAtclick(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+
+    }
---
+++ Assets/Scripts/General/MusicManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+    
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+ 
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+
+    //Play music according to level and our public AudioClip array clip
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[scene.buildIndex];
+        if (thisLevelMusic && audioSource.clip != thisLevelMusic)
+        {
+            Debug.Log("start music");
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+            
+           
+        
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
